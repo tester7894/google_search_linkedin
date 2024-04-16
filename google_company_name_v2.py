a9db6f0b1651af67fdd8c7450dc85903b1696c0e
tester7894 <167072562+tester7894@users.noreply.github.com>
2024-04-16 16:22:49 +0400

    Update google_company_name_v2.py

--- google_company_name_v2.py
+++ google_company_name_v2.py
@@ -23,12 +23,19 @@ with open('list_domain.csv', 'r') as file:
 		enter_search.send_keys(Keys.ENTER)
 		time.sleep(2)
 
+		num=0
+		while num < 5:
+			num+=1
+			time.sleep(1)
+			driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
 		links = driver.find_elements(By.TAG_NAME, 'h3')
 		for link in links:
 			name_company = (link.text)
 			print(name_company)
 			with open('result_1.txt', 'a+') as file:
-				file.write(f'{name_company}\n')
+				if name_company != 'Ещё результаты':
+					file.write(f'{name_company}\n')
 
 		time.sleep(3)
 
