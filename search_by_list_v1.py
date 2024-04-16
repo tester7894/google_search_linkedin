f9d5e2ea8b341b8e210c9dce5c7267670f48775e
tester7894 <167072562+tester7894@users.noreply.github.com>
2024-04-16 16:23:33 +0400

    Update search_by_list_v1.py

--- search_by_list_v1.py
+++ search_by_list_v1.py
@@ -9,14 +9,20 @@ driver.get('http://google.com')
 driver.maximize_window()
 input('Нажми кнопку для продолжения...')
 
+
 with open('result_1.txt', 'r') as file:
     persons = file.readlines()
+    num=0
     for person in persons:
         if person != '':
+            num+=1
             print(person)
             driver.get('http://google.com')
             enter_search = driver.find_element(By.TAG_NAME, 'textarea')
+
             enter_search.send_keys(f'site:linkedin.com/in intitle:{person}')
+            if num == 1:
+                input('Нажми, как будешь готов...')
             try:
                 enter_search.send_keys(Keys.ENTER)
             except:
